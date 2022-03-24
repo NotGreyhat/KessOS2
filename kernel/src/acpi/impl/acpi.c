cb65374d3042768a78118c567c05a12cbd707ee7
NotGreyhat <notgreyhat@gmail.com>
2022-03-23 21:17:21 -0400

    Ignore revision.

--- kernel/src/acpi/impl/acpi.c
+++ kernel/src/acpi/impl/acpi.c
@@ -2,21 +2,18 @@
 
 
 acpi_madt_t* madt = NULL;
-bool legacy = false;
 
 void acpi_init(struct stivale2_struct_tag_rsdp* rsdp_tag) {
     acpi_rsdp_t* rsdp = (acpi_rsdp_t*)rsdp_tag->rsdp;        
     acpi_rsdt_t* rsdt = (acpi_rsdt_t*)(uint64_t)rsdp->rsdtaddr;
 
-    if (rsdp->rev == 0) {
-        legacy = true;
-    } else {
-        volatile unsigned short tableidx = 0;
-        while (tableidx < rsdt->header.length - sizeof(rsdt->header) / 4) {
-            acpi_madt_t* _madt = (acpi_madt_t*)(uint64_t)rsdt->tables[tableidx];
-            if (strncmp(_madt->header.signature, "APIC", 4)) {
-                madt = _madt;
-            }
+    volatile unsigned short tableidx = 0;
+    while (tableidx < rsdt->header.length - sizeof(rsdt->header) / 4) {
+        acpi_madt_t* _madt = (acpi_madt_t*)(uint64_t)rsdt->tables[tableidx];
+        if (strncmp(_madt->header.signature, "APIC", 4)) {
+            madt = _madt;
         }
+
+        ++tableidx;
     }
 }
